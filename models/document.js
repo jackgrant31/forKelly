3eccafd5052479bda2378ff051145ed2401f2c6c
Tim Christovich <christ10@my.erau.edu>
2019-06-11 09:35:23 -0400

    Add queries for documents

--- models/document.js
+++ models/document.js
@@ -6,7 +6,7 @@ const DocumentSchema = mongoose.Schema ({
     _id: { type: Number, required: true },
     scanId: {type: String, required: true},
     courseId: {type: String, required: true},
-    status: {type: String, required: true},
+    status: {type: Object, required: true},
     name: {type: String, required: true},
     uploadDate: {type: String, required: true},
     scanDate: {type: String, required: true}
@@ -16,13 +16,28 @@ const Document = module.exports = mongoose.model('Document', DocumentSchema);
 
 module.exports.getAll = function(callback) {
     const query = {};
-    Course.find(query, callback);
+    Document.find(query, callback);
 }
 
-module.exports.getCourse = function(id, callback) {
-    Course.findById(id, callback);
+module.exports.getDocument = function(id, callback) {
+    Document.findById(id, callback);
 }
 
-module.exports.addCourse = function(newCourse, callback) {
-    newCourse.save(callback);
+module.exports.getDocumentsByCourse = function(courseId, callback) {
+    const query = {courseId: courseId, 'status.reviewed': false };
+    Document.find(query, callback);
+}
+
+module.exports.getDocumentFromCourse = function(courseId, documentId, callback) {
+    const query = {courseId: courseId, _id: documentId};
+    Document.findOne(query, callback);
+}
+
+module.exports.updateDocumentReviewStatus = function(documentId, newState, callback) {
+    const query = { _id: documentId };
+    Document.findOneAndUpdate(query, { status: newState }, callback);
+}
+
+module.exports.addDocument = function(newDocument, callback) {
+    newDocument.save(callback);
 }
