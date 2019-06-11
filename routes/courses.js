d8a98a2a89560cc12e8606485fa9d3da630f93c3
Tim Christovich <christ10@my.erau.edu>
2019-06-11 09:35:51 -0400

    Update course routes to handle document retrieval

--- routes/courses.js
+++ routes/courses.js
@@ -5,11 +5,11 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
 const Course = require('../models/course');
+const Document = require('../models/document');
 const Token = require('../models/verificationToken')
 var crypto = require('crypto');
 var nodemailer = require('nodemailer');
 
-// Profile
 router.get('/all', passport.authenticate('jwt', {
     session: false
 }), (req, res, next) => {
@@ -60,6 +60,104 @@ router.get('/:tagId', passport.authenticate('jwt', {
         }
     });
     
+    // res.send("tagId is set to " + req.params.tagId);
+});
+
+router.get('/:tagId/documents', passport.authenticate('jwt', {
+    session: false
+}), (req, res, next) => {
+    Document.getDocumentsByCourse(req.params.tagId, (err, documents) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                msg: 'No course found with that ID'
+            });
+        }
+
+        if (documents) {
+            return res.status(200).send({
+                data: documents,
+                success: true
+            });
+        } else {
+            res.status(500).json({
+                success: false,
+                msg: 'Something went wrong'
+            });
+        }
+    });
+
+    // res.send("tagId is set to " + req.params.tagId);
+});
+
+router.get('/:tagId/document/:docId', passport.authenticate('jwt', {
+    session: false
+}), (req, res, next) => {
+    Document.getDocumentFromCourse(req.params.tagId, req.params.docId, (err, document) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                msg: 'No course found with that ID'
+            });
+        }
+
+        if (document) {
+            return res.status(200).send({
+                data: document,
+                success: true
+            });
+        } else {
+            res.status(500).json({
+                success: false,
+                msg: 'Something went wrong'
+            });
+        }
+    });
+
+    // res.send("tagId is set to " + req.params.tagId);
+});
+
+router.post('/:tagId/document/:docId', passport.authenticate('jwt', {
+    session: false
+}), (req, res, next) => {
+    try {
+        let updatedState = {
+            "reviewed": true,
+            "reported": req.body.reported
+        };
+
+        if(req.body.reported == true){
+            console.log("Submitting takedown form request...");
+            /**
+             * @TODO
+             * 
+             * This is where you will need to call your Selenium 
+             * code to fill the form for demonstration purposes only
+             */
+        }
+
+        Document.updateDocumentReviewStatus(req.params.docId, updatedState, (err, document) => {
+            if (err) res.status(500).json({
+                success: false,
+                msg: 'Failed to update document'
+            });
+            else {
+                res.status(200).json({
+                    success: true,
+                    msg: "Successfully updated document"
+                });
+                // });
+            }
+        });
+    } catch (e) {
+        //this will eventually be handled by your error handling middleware
+        res.json({
+            success: false,
+            msg: `Encountered and Unknown error: ${e}`
+        })
+        next(e)
+    }
+
     // res.send("tagId is set to " + req.params.tagId);
 });
 
